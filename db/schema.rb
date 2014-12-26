80c4251e16dbc4b15c30f7687357cb6360ed9d69
saicharan2410 <saicharan2410@gmail.com>
2014-12-25 22:02:47 -0600

    pgupdate
    database change

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141226034757) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.string   "format"
